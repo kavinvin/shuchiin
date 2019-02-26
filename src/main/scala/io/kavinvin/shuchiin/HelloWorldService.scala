b65c39e9f573984df9374b05c8d7c894769ad3ff
kavinvin <kavinvin.vin@gmail.com>
2019-02-26 11:27:42 +0700

    Create http4s project

---
+++ src/main/scala/io/kavinvin/shuchiin/HelloWorldService.scala
@@ -0,0 +1,19 @@
+package io.kavinvin.shuchiin
+
+import cats.effect.Effect
+import io.circe.Json
+import org.http4s.HttpService
+import org.http4s.circe._
+import org.http4s.dsl.Http4sDsl
+
+import scala.language.higherKinds
+
+class HelloWorldService[F[_]: Effect] extends Http4sDsl[F] {
+
+  val service: HttpService[F] = {
+    HttpService[F] {
+      case GET -> Root / "hello" / name =>
+        Ok(Json.obj("message" -> Json.fromString(s"Hello, $name")))
+    }
+  }
+}
